9b9706a930d9fcb1ec5bf47d101b67aa98434245
Xara <xaratanha@gmail.com>
2023-01-23 17:22:38 -0800

    Add effects for clicking on years

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import DatePicker from "./components/datePicker/DatePicker.js";
+import {useState} from "react";
 
 function App() {
+    const [ date,setDate ] = useState(Date.now());
   return (
     <div className="App">
         salam
-      <DatePicker date={Date.now()} show={true}/>
+      <DatePicker date={ date } show={ true } setDate={ setDate }/>
     </div>
   );
 }
--- src/components/datePicker/yearMode/YearMode.js
+++ src/components/datePicker/yearMode/YearMode.js
@@ -1,21 +1,49 @@
 import {daysInMonth, getMonthStartDay, range} from "../utilities.js";
 import {useEffect, useRef, useState} from "react";
+import {useTheme} from "../../../ThemeContext.js";
 
 function YearMode(props) {
     const year = useRef(0);
+    const [colors] =useTheme();
     const [ cells,setCells ] = useState([]);
+    const [ selected,setSelected ] = useState(0);
+    const styles = {
+        now:{
+            backgroundColor: colors.primary,
+            color:colors.onPrimary,
+        },
+        selected:{
+            border: `1px solid ${ colors.primary }`,
+        }
+    }
 
+    function handleYearChange(year){
+        setSelected(year)
+        const tempDate = new Date(props.date);
+        const month = tempDate.getMonth();
+        const day =tempDate.getDate();
+
+        props.setDate(new Date(year,month,day))
+    }
     useEffect(()=>{
         year.current = new Date(props.date).getFullYear();
         setCells(range(year.current-6,year.current+8));
-    },[props.date,year])
+    },[ props.date,year ])
 
-    console.log(cells)
+    console.log(selected)
     return (
         <div className="years" style={{}}>
             <div className="years-grid">
-                { cells.map(x=>
-                    <div className='cell'>
+                { cells.map( x=>
+                    <div className='cell'
+                         onClick={ ()=>handleYearChange(x) }
+                         style={ x === selected
+                                    ? styles.selected
+                                    : x === year.current
+                                        ? styles.now
+                                        : {}
+                                }
+                    >
                         {x}
                     </div>
                 )}
