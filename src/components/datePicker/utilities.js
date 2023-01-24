9c263d821bc2eddd9d71d3c2d2897520718dccc3
Xara <xaratanha@gmail.com>
2023-01-24 09:47:02 -0800

    Create monthName function

--- src/components/datePicker/utilities.js
+++ src/components/datePicker/utilities.js
@@ -22,4 +22,8 @@ function hexToRGB(hex){
     const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result ? `${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}` : null;
 }
+function monthName(number) {
+    const months= ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    return months[number]
+}
 export {daysInMonth,getMonthStartDay,range,hexToRGB}
