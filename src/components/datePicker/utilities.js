574212771b42df5c717a89eb2323e04e7b5c8e0f
Xara <xaratanha@gmail.com>
2023-01-23 19:20:17 -0800

    Create function hexToRGB

--- src/components/datePicker/utilities.js
+++ src/components/datePicker/utilities.js
@@ -12,4 +12,14 @@ function getMonthStartDay(timestamp) {
     return date.getUTCDay(); // get the day of the week (0-6, where 0 is Sunday)
 }
 const range = (start, end) => [...Array(end - start + 1).keys()].map(x => x + start);
+
+function hexToRGB(hex){
+    const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? `${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}` : null;
+}
 export {daysInMonth,getMonthStartDay,range}
