93d6a90cfe932478f5e2c352920cc067c2ea5664
Xara <xaratanha@gmail.com>
2023-01-25 12:37:26 -0800

    Rename functions

--- src/components/datePicker/Actions.js
+++ src/components/datePicker/Actions.js
@@ -1,7 +1,21 @@
+import Button from "../button/Button.js";
+
 function Actions() {
     return (
-        <>
-        </>
+        <div className="actions">
+            <div className='btns-container'>
+                <Button type={ 'text' }
+                        click={ props.onClickLeftOne }
+                >
+                    Cancel
+                </Button>
+                <Button type={ 'text' }
+                        click={ props.onClickRightOne }
+                >
+                    OK
+                </Button>
+            </div>
+        </div>
     )
 }
 export default Actions;
