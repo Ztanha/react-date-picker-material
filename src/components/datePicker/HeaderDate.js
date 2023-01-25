6ad0e2980efc7fcdee18e82cfc351fff020c3cda
Xara <xaratanha@gmail.com>
2023-01-25 12:05:30 -0800

    Add styles

--- src/components/datePicker/HeaderDate.js
+++ src/components/datePicker/HeaderDate.js
@@ -1,7 +1,37 @@
-function HeaderDate(){
+import {monthName} from "./utilities.js";
+import {ReactComponent as Arrow} from "../icons/right.svg";
+import {ReactComponent as SideArrow} from "../icons/sideArrow.svg";
+
+function HeaderDate( props ){
+    const style={
+        icons:{
+            fill:colors.onSurfaceVariant
+        },
+        smallDate:{
+            color:colors.onSurfaceVariant
+        }
+    }
     return (
         <>
-
+            <div className="date"
+                 onClick={ ()=>props.setMode('years') }
+                 style={ styles.smallDate }
+            >
+                { monthName(refMonth)},{ refYear }
+                <Arrow className='icon-down'
+                       style={ styles.icons }
+                />
+            </div>
+            <div className="icons">
+                <SideArrow style={ styles.icons }
+                           className="icon-left"
+                           onClick={ props.onClickBackward }
+                />
+                <SideArrow style={ styles.icons }
+                           className="icon-right"
+                           onClick={ props.onClickForward }
+                />
+            </div>
         </>
     )
 }
