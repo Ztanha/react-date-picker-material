21c58e635d2a3d81a68b76849ef6a580282ce07d
Xara <xaratanha@gmail.com>
2023-01-25 12:21:00 -0800

    Add headerDate component

--- src/components/datePicker/yearMode/YearMode.js
+++ src/components/datePicker/yearMode/YearMode.js
@@ -4,6 +4,7 @@ import {useTheme} from "../../../ThemeContext.js";
 import Button from "../../button/Button.js";
 import {ReactComponent as Arrow} from "../icons/right.svg";
 import {ReactComponent as SideArrow} from "../icons/sideArrow.svg";
+import HeaderDate from "../HeaderDate.js";
 
 function YearMode(props) {
     const [colors] = useTheme();
@@ -39,27 +40,33 @@ function YearMode(props) {
     },[props.year])
 
     return (<>
-        <div className="localSelectionRow grid-order">
-            <div className="date"
-                 onClick={ ()=>props.setMode('days') }
-                 style={styles.smallDate}
-            >
-                { monthName(props.month) },{ selectedYear || props.year }
-                <Arrow className='icon-down'
-                       style={ styles.icons }
-                />
-            </div>
-            <div className="icons">
-                <SideArrow style={ styles.icons }
-                           className="icon-left"
-                           onClick={ ()=>reloadCells(refYear.current-15)}
-                />
-                <SideArrow style={ styles.icons }
-                           className="icon-right"
-                           onClick={ ()=>reloadCells(refYear.current+15)}
-                />
-            </div>
-        </div>
+        <HeaderDate onClickForward={ ()=>reloadCells(refYear.current+15) }
+                    onClickBackward={ ()=>reloadCells(refYear.current-15) }
+                    onClickDown={ ()=>props.setMode('days') }
+                    month={ props.month }
+                    year={ (selectedYear || props.year) }
+        />
+        {/*<div className="localSelectionRow grid-order">*/}
+        {/*    <div className="date"*/}
+        {/*         onClick={ ()=>props.setMode('days') }*/}
+        {/*         style={styles.smallDate}*/}
+        {/*    >*/}
+        {/*        { monthName(props.month) },{ selectedYear || props.year }*/}
+        {/*        <Arrow className='icon-down'*/}
+        {/*               style={ styles.icons }*/}
+        {/*        />*/}
+        {/*    </div>*/}
+        {/*    <div className="icons">*/}
+        {/*        <SideArrow style={ styles.icons }*/}
+        {/*                   className="icon-left"*/}
+        {/*                   onClick={ ()=>reloadCells(refYear.current-15)}*/}
+        {/*        />*/}
+        {/*        <SideArrow style={ styles.icons }*/}
+        {/*                   className="icon-right"*/}
+        {/*                   onClick={ ()=>reloadCells(refYear.current+15)}*/}
+        {/*        />*/}
+        {/*    </div>*/}
+        {/*</div>*/}
         <div className="years" style={{}}>
             <div className="years-grid">
                 { cells.map( x=>
