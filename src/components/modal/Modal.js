80126978647d2a0d55eb55514a35f2d88c95ba56
Xara <xaratanha@gmail.com>
2023-01-26 11:57:03 -0800

    Make modal z-index handable with props and also internal style

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,27 +1,40 @@
 
 import './modal.scss'
 import {useTheme} from "../../ThemeContext.js";
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 
 function Modal( props ) {
     const [colors]=useTheme();
+    const modal=useRef();
+    const scrim=useRef();
+
     const internalStyle={
-        width:props.width+'px'
+        zIndex: 1001
     }
+
+    useEffect(()=>{
+        if( modal.current ) {
+            let zIndex= parseInt( window.getComputedStyle( modal.current ).zIndex);
+            scrim.current.style.zIndex = zIndex - 1;
+        }
+    },[ modal,scrim ])
+
     return (
         props.show ? (
             <div className='modal-component'>
                 <div
                     className='scrim'
                     onClick={ props.hide }
+                    ref={ scrim }
                     style={{
-                        backgroundColor:`${colors.scrim}`
+                        backgroundColor:`${ colors.scrim }`
                     }}
 
                 />
                 <div
-                    id='modal-container'
-                    style={{...internalStyle,...props.style}}
+                    className='modal-container'
+                    ref={ modal }
+                    style={{ ...internalStyle,...props.style }}
                 >
                     { props.children }
 
